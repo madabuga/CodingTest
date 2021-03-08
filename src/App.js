3fb707a9a0fb8bcbe5ad9fd1454a12bef9f7fd73
Madalina Buga <mada.buga@yahoo.com>
2021-03-08 23:45:13 +0200

    Added Apollo and returned posts with GraphQL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Posts from './components/Posts';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const client = new ApolloClient({
+  uri: "https://fakerql.stephix.uk/graphql"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <Posts />
     </div>
-  );
-}
+  </ApolloProvider>
+)
 
 export default App;
